77cc247bb3e2fa1248e0c48ebce65adca9a17d31
Kamrul Hasan <kamrul.hasan@technovista.com.bd>
2018-03-07 12:12:54 +0600

    db updated

--- static/new_census.js
+++ static/new_census.js
@@ -2,34 +2,34 @@
  * Created by Nazmul on 3/1/2018.
  */
 jQuery(document).ready(function($) {
-    var select2_control = $('.select');
-    select2_control.select2({
-        //placeholder:'Select an option'
-    });
-    $('.select-size-xs').select2({
-        containerCssClass: 'select-xs'
-    });
+    // var select2_control = $('.select');
+    // select2_control.select2({
+    //     //placeholder:'Select an option'
+    // });
+    // $('.select-size-xs').select2({
+    //     containerCssClass: 'select-xs'
+    // });
 
-    $('.select2-sm').select2({
-        containerCssClass: 'select2-sm'
-    });
+    // $('.select2-sm').select2({
+    //     containerCssClass: 'select2-sm'
+    // });
 
-    $('.select2-md').select2({
-        containerCssClass: 'select2-md'
-    });
+    // $('.select2-md').select2({
+    //     containerCssClass: 'select2-md'
+    // });
 
-    $('.select2-lg').select2({
-        containerCssClass: 'select2-lg'
-    });
-    // Open the fields onFocus
-    // THE ORDER IS IMPORTANT.
-    function select2Focus() {
-        $(this).closest('.select2').prev('select').select2('open');
-    }
+    // $('.select2-lg').select2({
+    //     containerCssClass: 'select2-lg'
+    // });
+    // // Open the fields onFocus
+    // // THE ORDER IS IMPORTANT.
+    // function select2Focus() {
+    //     $(this).closest('.select2').prev('select').select2('open');
+    // }
 
-    select2_control.next('.select2').find('.select2-selection').one('focus', select2Focus).on('blur', function () {
-        $(this).one('focus', select2Focus);
-    });
+    // select2_control.next('.select2').find('.select2-selection').one('focus', select2Focus).on('blur', function () {
+    //     $(this).one('focus', select2Focus);
+    // });
 
     /*$('#district-id').on('change', function() {
         $('#district-id').trigger('change');
